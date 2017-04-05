1d297b821d07e007f539665a6410ab513df18ec6
Aaron Bray <aaron.m.bray@gmail.com>
2017-04-05 08:58:47 -0500

    Exception stuff added

--- DotNetReflectiveLoading/main.cpp
+++ DotNetReflectiveLoading/main.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
 #include "clr.hpp"
 
+void callback0(const wchar_t* p)
+{
+    std::wcout << L"Callback 0: Message: " << p << std::endl;
+}
+
+void callback1(const wchar_t* p)
+{
+    std::wcout << L"Callback 1: " << p << std::endl;
+    auto tmp = *((char*)0);
+}
+
+
+class CppClassEx {
+public:
+    int fn() {
+        return 10;
+    }
+
+    int excepted(const wchar_t* p) {
+        std::wcout << L"Hello from unmanaged C++ land! " << p << std::endl;
+        throw std::runtime_error("Failsauce!");
+        return 20;
+    }
+};
 
 int wmain(int argc, wchar_t** argv)
 {
     DWORD bytes = 0;
     std::vector<uint8_t> vec;
-    if (argc < 3) {
-        std::cout << "Please use:\nDotNetReflectiveLoading.exe <assembly_path> <class_name>" << std::endl;
-    }
         
-    auto hf = CreateFileW(argv[1], GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+    auto hf = CreateFileW(L"DemoAssembly.dll", GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
     if (INVALID_HANDLE_VALUE == hf)
         return 0;
 
@@ -33,16 +54,42 @@ int wmain(int argc, wchar_t** argv)
         std::cout << "Failed to load module!" << std::endl;
         return -2;
     }
+    
+    res->invoke_static(L"DemoAssembly.Demo", L"Popup", L"Hah, Messagebox!");
 
-    res->invoke_static(L"TestLib.Class1", L"drop_file", L"derp.txt", L"Herp Contents");
-
-    auto cls = res->construct(argv[2]);
+    auto cls = res->construct(L"DemoAssembly.Demo");
     if(!cls) {
         std::cout << "Class construction failed!" << std::endl;
         return -3;
     }
 
-    cls->invoke_method(L"mb", L"This is a test");
+    LONGLONG fp = (LONGLONG)callback0;
+    cls->invoke_method(L"Callback", fp);
+    auto var = cls->invoke_method(L"GetString");
+    auto bst = static_cast<bstr_t>(var);
+    std::wcout << L"Message from CLR: " << static_cast<const wchar_t*>(bst) << std::endl;
+
+    fp = (LONGLONG)callback1;
+    cls->invoke_method(L"ExceptionCallback", fp);
+
+    CppClassEx example;
+
+    auto r = cls->invoke_method(L"AddClass", ((LONGLONG)&example));
+    if (!static_cast<bool>(r))
+        return -4;
+
+    union {
+        decltype(&CppClassEx::fn) f;
+        decltype(&CppClassEx::excepted) ex;
+        void* p;
+    } u;
+
+    u.f = &CppClassEx::fn;
+
+    cls->invoke_method(L"InvokeMethod", ((LONGLONG)(u.p)));
+
+    u.ex = &CppClassEx::excepted;
+    cls->invoke_method(L"InvokeExcepted", ((LONGLONG)u.p));
 
     system("pause");
     return 0;
